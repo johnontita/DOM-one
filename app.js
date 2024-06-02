d1f7992d6649f74521931bd288454f24fb807437
john ontita <johnontita2000@gmail.com>
2024-06-02 22:09:08 +0300

    implementing Data object Model using textcontent and value propertis

---
+++ app.js
@@ -0,0 +1,10 @@
+// Document object method(DOM) is a connector between Js and html and css 
+console.log(document.querySelector('.message'));
+console.log(document.querySelector('.message').textContent); //last dot will display te content
+document.querySelector('.message').textContent='Correct score'
+document.querySelector('.value').textContent=13
+document.querySelector('.score').textContent=5
+
+document.querySelector('.input').value=15
+console.log(document.querySelector('.input').value)
+
